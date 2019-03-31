12e11fd3cddd197e7edd4fedcd700805cf822698
Emigrant Dolphin <liutas58@gmail.com>
2019-03-31 21:29:31 +0300

    improved windows compile/handling

--- WinCompile.bat
+++ WinCompile.bat
@@ -2,16 +2,50 @@
 rem enables the use of !, ! makes so var can be expanded dynamically
 setlocal EnableDelayedExpansion 
 
+if "%1" == "compile" goto COMPILE
+if "%1" == "makeInclude" goto MAKEINCLUDE
+if "%1" == "delInclude" goto DELINCLUDE
+if "%1" == "help" goto HELP
+goto EOF
+
+:COMPILE
 rem  Need to send every .cpp file to compiler. So I make a src variable and fill it with all .cpp files that are in src folder
 set src=
 for %%f in (src\*.cpp) do set src=!src! %%f
 
 rem Using -I to include all header files in incude folder for compilation. Also add src variable at the end to distinguish these files as mine
 set myFiles=-Iinclude %src%
-echo %src%
+
 rem Linking sfml libraries to compilator
 set sfml=-LSFML/lib -l:libsfml-system.a -l:libsfml-window.a -l:libsfml-graphics.a
 
-g++ main.cpp  -o BattleShips.exe -std=c++14 %myFiles% %sfml%
+if "%2" == "" g++ main.cpp  -o BattleShipsW.exe -std=c++14 %myFiles% %sfml%
 
-if "%1" == "run" BattleShips.exe
+if "%2" == "run" g++ main.cpp  -o BattleShipsW.exe -std=c++14 %myFiles% %sfml% && BattleShipsW.exe
+
+goto EOF
+
+:MAKEINCLUDE
+if "%2" == "" goto EOF
+
+type nul > include\%2.h
+type nul > src\%2.cpp
+notepad++ include\%2.h src\%2.cpp
+
+goto EOF
+
+:DELINCLUDE
+
+del include\%2.h
+del src\%2.cpp
+
+goto EOF
+
+:HELP
+echo compile or compile run
+echo makeInclude name
+echo delInclude name
+
+goto EOF
+
+:EOF
